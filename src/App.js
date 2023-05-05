d887747b20e6b36e48aeefe1b312b84debb27c3f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-05 17:31:46 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="calc-grid">
-      <div className='output'>
-        <div className='prev-operand'></div>
-        <div className='curr-operand'></div>
-      </div>
-      <button className='span-two'>AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className='span-two'>=</button>
-    </div>
+  <div><h1>Hello World</h1></div>
   );
 }
 
